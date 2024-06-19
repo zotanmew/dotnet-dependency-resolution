2e5453a479d9e3f27a018ee9f46c2429895f8ecb
Laura Hausmann <laura@hausmann.dev>
2024-06-19 19:48:33 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 hash=$(sha256sum "libvips.so.42")
-hash=${hash/ libvips.so.42/}
+hash=${hash/  libvips.so.42/}
 if [[ "$hash" == "d9a06f7adce8a07213c68ba0be7046a255d2952893174fed5b1187589e770aa2" ]]; then
 	echo "arm64-glibc libvips.so.42 detected"
 elif [[ "$hash" == "3aa9e0d2865e8a65a0d4d7fa4011e1ab2b6e5c313b53d7584363da817db2b45f" ]]; then
